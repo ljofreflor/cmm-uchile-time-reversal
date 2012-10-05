ffb23e5add51cbef7f57e6f74058cc5397105014
Leonardo <ljofre2146@gmail.com>
2012-10-05 01:01:04 -0300

    Update @Event/Event.m
    Definición de la clase evento, que abstrae el un sensor con sus múltiples sismógrafos.

--- @Event/Event.m
+++ @Event/Event.m
@@ -1,7 +1,9 @@
 classdef Event < handle
-    %MODEL Summary of this class goes here
-    %   Detailed explanation goes here
+    %MODEL Clase que abstrae un evento s`ismico. Esta clase contiene un conjunto de par`ametros
+    % correspondiente al evento en si, como el tiempo estimado por codelco y la posici'on en los
+    % set de datos dados en los documentos
     properties
+        eventName
         sp_valid
         cordenadasValidad
         Syncdt
