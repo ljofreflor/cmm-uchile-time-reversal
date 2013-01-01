90186e867c20b45f2b7c1dff0a01608bb3261531
leonardo <leonardo@leonardo-N53SV.(none)>
2012-12-31 23:19:02 -0300

    cambios a la documentacion

--- matlab/scripts/windowsErase.m
+++ matlab/scripts/windowsErase.m
@@ -1,23 +1,18 @@
-function mu =  windowsErase(mu)
+function ev =  windowsErase(ev)
 %En este script conservamos solamente la sen~al de los geosensores
 %que se encuentra en una ventana ligeramente mas ancha que la ventana
 %correspondiente al intervalo ideal de la onda P y la onda S.
 
-for kk = 1:mu.count
+for kk = 1:ev.count
     
-    sens = mu.gss(kk);
-    
+    sens = ev.gss(kk);
     p_time = sens.p_time;
     s_time = sens.s_time;
     t_time = sens.t_time;
-    
     n = size(sens.data,1);
-    
     TriggerPosition = sens.TriggerPosition;
-    Fs = mu.gss(kk).hardware_sampling_rate;
-    
+    Fs = ev.gss(kk).hardware_sampling_rate;
     delta_ps = s_time - p_time;
-    
     lowerIndex = min(n, max( 1 , TriggerPosition + ...
         round(Fs*( p_time - .1*delta_ps - t_time))));
     
@@ -27,14 +22,19 @@ for kk = 1:mu.count
     % anular las mediciones de los sensores en los cuales la diferencia del
     % tiempo de llegada de la p y de la s no es válido, el sistema mediante
     % la inversa generalizada tratar'a esos datos en el sistema matricial
-    % obteniendo siempre soluci`on.
+    % obteniendo siempre solucionn.
     
-    if mu.gss(kk).diferenciaPSvalida
-        mu.gss(kk).data(1:lowerIndex,:) = 0;
-        mu.gss(kk).data(uperIndex:n,:) = 0;
+    if ev.gss(kk).diferenciaPSvalida
+        % eliminar las mediciones fuera del intervalo definido como el que
+        % tiene la señal
+        ev.gss(kk).data(uperIndex:n,:) = [];
+        ev.gss(kk).data(1:lowerIndex,:) = [];
+        
+        ev.gss(kk).timevector(uperIndex:n) = [];
+        ev.gss(kk).timevector(1:lowerIndex) = [];        
     else
-        mu.gss(kk).data(:,:) = 0;
-        mu.gss(kk).data(:,:) = 0;
+        ev.gss(kk).data(:,:) = [];
+        ev.gss(kk).data(:,:) = [];
     end
 end
 end
